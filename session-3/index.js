2c47244fbd6a35e264656d33811b02aa219cbdf4
Rebecca Boateng <r.y.boateng@outlook.com>
2022-10-10 19:58:03 +0100

    invoke function

--- session-3/index.js
+++ session-3/index.js
@@ -1,2 +1,22 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+
+let a = 5;
+const b = 10;
+let c = a + b;
+c = 0;
+
+console.log ( a + b  + c);
+
+function sayHey () { 
+    console.log ( "Hey!" ); 
+}
+
+function conversation () { 
+    sayHey ();
+    console.log ( "How are you?" );
+    console.log ( "Goodbye" );
+ 
+}
+
+conversation ();
