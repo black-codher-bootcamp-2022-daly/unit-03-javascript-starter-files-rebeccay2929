ee9ff0d677b18f68bc177b38855c53ec9b59a777
Rebecca Boateng <r.y.boateng@outlook.com>
2022-10-22 21:51:36 +0100

    session-5

---
+++ session-5/index.js
@@ -0,0 +1,58 @@
+// function getGrade (total) {
+// if (total > 90) {
+//     console.log('A+')
+// }
+// else if (total >80 && total <=90) {
+//     console.log ('A')
+//     }
+// else if (total >70 && total <=80) {
+//     console.log ('B')
+// }
+// else if (total >60 && total <=70) {
+//     console.log ('C')
+// }
+// else if (total >50 && total <=60) {
+//     console.log ('D')
+// }
+// else if (total >40 && total <=50) {
+//     console.log ('E')
+// }
+// else if (total >30 && total <=40) {
+//     console.log ('F')
+// }
+// else if (total <30){
+//     console.log ('you have failed')
+// }
+// }
+
+// getGrade (55)
+
+
+function getGrade (total) {
+    switch (total > 90) {
+        case total >90:
+        console.log('A+');
+        break;
+    case total <60:
+        console.log ('you have failed');
+        break;
+    }
+}
+    
+    getGrade (66)
+
+
+// const person = {
+//     name: "Alex",
+//     age: 7,
+// };
+// if (person.age >18){ 
+//    console.log ("Alex is over 18 years old"); }
+// else { 
+//     console.log ("Alex is under 18 years old")
+// };
+
+
+
+
+
